8b0f92ece782292fda5d8567c9c7bc656132133b
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2024-09-25 10:09:09 +0100

    feat(endpoint): added endpoint for recipification

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from controllers.recipify import recipify
 
 app = Flask(__name__)
 CORS(app)
@@ -11,8 +12,18 @@ class Welcome(Resource):
     def get(self):
         return "Welcome to the LLM Service"
 
+class Recipify(Resource):
+    def post(self):
+        data = request.get_json()
+        if 'recipe' in data:
+            recipe = data['recipe']
+            recipe_obj = recipify(recipe)
+            print(recipe_obj)
+            return jsonify({"recipe:": recipe_obj})
+
 
 api.add_resource(Welcome, "/api")
+api.add_resource(Recipify, "/api/recipe")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='3001',debug=True)
